90c6376d0d7be9e07a483c3a7a18b23a1ce8efe4
Almo <117903577+Alm036@users.noreply.github.com>
2025-02-01 18:16:54 +0300

    Пр 1.1 завершенная

--- 422_Frantsuzov/MainWindow.xaml.cs
+++ 422_Frantsuzov/MainWindow.xaml.cs
@@ -13,16 +13,54 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace _422_Frantsuzov
+namespace Пр1_ПИТ_422_Frantsuzov
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow() { InitializeComponent(); }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            InitializeComponent();
+            MessageBoxResult result = MessageBox.Show(
+                "Вы уверены, что хотите выйти?",
+                "Подтверждение выхода",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question);
+            if (result == MessageBoxResult.No) e.Cancel = true; 
+        }
+
+        double F_Check(double x)
+        {
+            if (Checked_shx.IsChecked == true) return Math.Sinh(x);
+            else if (Checked_x_2.IsChecked == true) return Math.Pow(x, 2);
+            else return Math.Exp(x);
+        }
+
+        double CalculateResult(double f, double b, double x)
+        {
+            if (1 < x*b && x*b < 10) return Math.Exp(f);
+            else if (12 < x*b && x*b < 40) return Math.Sqrt(Math.Abs(f + 4*b));
+            else return b * Math.Pow(f, 2);
+        }
+
+        private void Calc_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double x = double.Parse(TextBox_x.Text);
+                double b = double.Parse(TextBox_b.Text);
+                double f = F_Check(x);
+                string res = CalculateResult(f, b, x).ToString();
+                TextBox_res.Text = res;
+            }
+            catch { MessageBox.Show("Ошибка ввода"); }
+        }
+
+        private void Clear_Button_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox_res.Text = "";
+            TextBox_x.Text = "";
+            TextBox_b.Text = "";
         }
     }
 }
